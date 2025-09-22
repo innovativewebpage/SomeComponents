d090c64ed105387d3bd8ed98bb152d8d21b0ec56
Omar Rizwan <innovativewebpage@gmail.com>
2025-09-22 16:12:54 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
 
 			<input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-			<button onClick={() => submitData()}>Submit</button>
+			<button onClick={() => submitData()}>Submit1</button>
 			<input type="text" value={data} />
 		</div>
 	);
--- src/MyProps2.js
+++ src/MyProps2.js
@@ -2,18 +2,15 @@ import React from 'react';
 
 
 
-function Myprops2(props)
-{
-	
-	
-	return(
-			<div>
-			
-					<h3>Myprops2</h3>
-					<h3>{props.text2}</h3>
-			</div>
-	
-	
+function Myprops2(props) {
+	return (
+		<div>
+
+			<h3>Myprops32</h3>
+			<h3>{props.text2}</h3>
+		</div>
+
+
 	)
 }
 
